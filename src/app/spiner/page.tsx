13f753b7867550a7e4d83ac65d055073298a0f74
MarihkaDevochka <cinanamberwan@proton.me>
2024-04-06 21:04:57 +0300

    fix spiner

--- src/app/spiner/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-import Spinner from '@/components/spiner'
-
-type Props = {}
-
-export default function page({}: Props) {
-  return <Spinner ></Spinner>
-}
--- src/components/spiner.tsx
+++ src/components/spiner.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+// import React, { createContext } from 'react'
 import { AnimatePresence, motion } from 'framer-motion'
 import { RotateCw } from 'lucide-react'
 
